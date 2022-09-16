95e1c9bd00796cebbcafbf2d8a1226d7dea4e4c8
FantomasI <e.strelchuk1.21@gmail.com>
2022-09-16 11:12:52 +0300

    end

--- news/views.py
+++ news/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import TextNews, NewsHeadline
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 def index(request):
@@ -32,3 +40,40 @@ def detail(request, new_id):
         }
     )
 
+app_url = "/news/"
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = app_url + "login/"
+    template_name = "reg/register.html"
+    def form_valid(self, form):
+        return super(RegisterFormView, self).form_valid(form)
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "reg/login.html"
+    success_url = app_url
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(app_url)
+
+class PasswordChangeView(FormView):
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+    success_url = app_url + 'login/'
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
